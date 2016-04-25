cfb85f2acc63da9220d5915afeebca709bd3974d
pcooper <pcooper@hesd.k12.ca.us>
2016-04-25 11:16:52 -0700

    I added tray application to launch after install and codeplex documentation page.

--- RansomwareDetectionService/ProjectInstaller.cs
+++ RansomwareDetectionService/ProjectInstaller.cs
@@ -29,5 +29,69 @@ namespace RansomwareDetectionService
             
             base.Install(stateSaver);
         }
+
+       
+        public override void Commit(IDictionary savedState)
+        {
+            base.Commit(savedState);
+            try
+            {
+                if (System.IO.Directory.Exists("C:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("C:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("D:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("D:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("E:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("E:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("F:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("F:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("G:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("G:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("H:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("H:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("I:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("I:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("J:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("J:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("K:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("K:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("L:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("L:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("M:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("M:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+                else if (System.IO.Directory.Exists("N:\\Program Files (x86)\\Ransomware Detection Service"))
+                {
+                    System.Diagnostics.Process.Start("N:\\Program Files (x86)\\Ransomware Detection Service\\RansomwareDetectionSystemTray.exe");
+                }
+
+                System.Diagnostics.Process.Start("http://ransomwaredetectionservice.codeplex.com/documentation");
+            }
+            catch (Exception)
+            {
+    
+            }
+            
+        }
     }
 }
